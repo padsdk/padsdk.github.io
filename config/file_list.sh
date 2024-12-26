2c8375ad1089c7c43381123d5339530d0528e529
taugin <taugin@163.com>
2024-12-26 19:20:07 +0800

    1. upload maven files

--- config/file_list.sh
+++ config/file_list.sh
@@ -11,16 +11,18 @@ echo "JSON_FILE = $JSON_FILE"
 RELEASE="release/"
 SNAPSHOT="snapshot/"
 #FILES=$(find $RELEASE $SNAPSHOT -type f)
-FILES_RELEASE=$(find $RELEASE -name *.gradle -o -name *.aar -exec ls -t {} + | sort -r)
-FILES_SNAPSHOT=$(find $SNAPSHOT -name *.gradle -o -name *.aar -exec ls -t {} + | sort -r)
+FILES_RELEASE_GRADLE=$(find $RELEASE -name *.gradle -exec ls -t {} + | sort -r)
+FILES_RELEASE=$(find $RELEASE -name *.aar -exec ls -t {} + | sort -r)
+FILES_SNAPSHOT=$(find $SNAPSHOT -name *.aar -exec ls -t {} + | sort -r)
 #FILES+=("${FILES_SNAPSHOT[@]}")
-FILES=("${FILES_RELEASE[@]}" "${FILES_SNAPSHOT[@]}")
+#FILES=("${FILES_RELEASE[@]}" "${FILES_SNAPSHOT[@]}")
+FILES=$(echo "$FILES_RELEASE_GRADLE" "$FILES_RELEASE" "$FILES_SNAPSHOT")
 # 开始创建 JSON 文件
 echo "[" > $JSON_FILE
 
 # 遍历所有文件并写入 JSON
 FIRST=true
-for FILE in $FILES_SNAPSHOT; do
+for FILE in $FILES; do
   echo file=$FILE
   if [[ "${FILE##*.}" == "aar" ]] || [[ "${FILE##*.}" == "gradle" ]] ; then
     if [ "$FIRST" = true ]; then
