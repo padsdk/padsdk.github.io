4b5c0fb31463f00d80fff47d65391a15a2e38912
liuzhao.wei <liuzhao.wei@argonblue.tech>
2025-07-04 12:19:30 +0800

    1. upload maven files

--- upload.sh
+++ upload.sh
@@ -14,7 +14,7 @@ REMOTE_URL=$(git remote get-url origin)
 
 echo "REMOTE_URL = $REMOTE_URL"
 
-echo "WEB_PAGE = https://mobsdk-ads.github.io"
+echo "WEB_PAGE = https://padsdk.github.io"
 
 BRANCH=$(git branch)
 
