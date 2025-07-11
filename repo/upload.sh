c2772065d96100ad647856bb4c2da569ec33265d
liuzhao.wei <liuzhao.wei@argonblue.tech>
2025-07-11 18:56:50 +0800

    1. upload maven files

---
+++ repo/upload.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Upload script start to execute ..."
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+$SCRIPT_DIR/config/file_list.sh
+
+WORKDIR=$(pwd)
+
+echo "WORKDIR = $WORKDIR"
+
+REMOTE_URL=$(git remote get-url origin)
+
+echo "REMOTE_URL = $REMOTE_URL"
+
+echo "WEB_PAGE = https://padsdk.github.io"
+
+BRANCH=$(git branch)
+
+echo "BRANCH = $BRANCH"
+
+git add .
+
+git commit -m "1. upload maven files"
+
+echo "push $BRANCH -> $REMOTE_URL"
+
+git push -u origin HEAD
+
+echo "Upload script execute over ..."
